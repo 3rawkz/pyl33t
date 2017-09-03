35ae894350d0460e514111f6a4909786ec97cb52
3rawkz <e.rawkz@gmail.com>
2017-09-03 05:50:01 -0600

    Updates README.md
    Auto commit by GitBook Editor

--- README.md
+++ README.md
@@ -6,8 +6,5 @@
 
 This project is a simple leet-speak generator ; uses generators.com API and then returns the generated text to your clipboard. Quick and easy way to create leet-speak insert where you please.
 
-  
-
-
- Currently, this is a conceptual build, this Project's future plans are as follows
+Currently, this is a conceptual build, this Project's future plans are as follows
 
---
+++ pyl33t/main_testing.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import pyperclip
+import os
+import platform
+if platform.system() == 'Windows':
+    os.system('cls')
+else:
+    os.system('clear')
+
+#TODO: 
+#	A. Proper banner
+#	B. !LOOP/CALLBACK
+#   C. Create Class Genrator to pass to a tkinter function
+#		In TkApp
+#		1. Be able enter text
+#		2. Menu choose  font type
+
+greeting = ''' PYL33T... so you can geek while you speak.these are the fonts... \n 
+	0 = classic
+	1 = Ultra 
+	2 = Glyphtek
+	3 = Wolstein
+	4 = Ancy
+	5 = Beey
+	6 = Broadway
+	7 = fullwidth'''
+
+
+class const:
+
+	def __init__(self, font, text):
+		self.text = text
+		self.font = font
+
+	def txt_convert(self, font, text):
+		parameters = {"genr8rsUserIpAddress": "null", "genr8rsUserId": "null","_sCharacterSet": self.font, "_sText": self.text}
+		response = requests.get("http://api.genr8rs.com/Generator/Fun/LeetSpeakGenerator?", params=parameters)
+
+		data = response.json()
+		pyperclip.copy(data["_sResult"])
+		print("TEXT : " + data["_sResult"])
+
+
+done = False
+while done == False:
+	i = ['classic', 'ultra', 'glyphtek', 'wolfenstein', 'ancy', 'beefy', 'broadway', 'fullwidth']
+	print(greeting)
+	txt = input('\nENTER TEXT: \n')
+	fnt = int(input('FONT? \n'))
+	fnt = i[fnt]
+
+	t = const(fnt, txt)
+	t.txt_convert(fnt, txt)
+	n = input('Q FOR QUIT OR ENTER TO CONTINUE')
+	if n.lower() == "q":
+		done = True
+		if done == True:
+			quit()
